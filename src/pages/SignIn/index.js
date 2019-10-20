f37d976cb4bbfe6d97c36d53fda1f0551655201e
Hugo Cucco <hugo_cs22@hotmail.com>
2019-10-20 19:33:31 -0300

    first commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  username: Yup.string().required('Username obrigatório'),
+  password: Yup.string().required('A Senha é obrigatória'),
+});
+
+export default function SignIn() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ username, password }) {
+    dispatch(signInRequest(username, password));
+  }
+  return (
+    <>
+      <img src={logo} alt="Detetive" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="username" type="username" placeholder="Username" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <button type="submit">{loading ? 'Carregando...' : 'Entrar'}</button>
+      </Form>
+    </>
+  );
+}
---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+  padding-bottom: 100px;
+
+  strong {
+    color: #fff;
+    font-size: 23px;
+  }
+
+  hr {
+    border: 0;
+    height: 1px;
+    background: rgba(255, 255, 255, 0);
+    margin: 10px 0 5px;
+  }
+
+  img {
+    width: 45%;
+    padding-bottom: 15px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      collor: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.9);
+      }
+    }
+
+    span {
+      color: #8b0000;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #b22222;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.3s;
+
+      &:hover {
+        background: ${darken(0.2, '#B22222')};
+      }
+    }
+  }
+`;
